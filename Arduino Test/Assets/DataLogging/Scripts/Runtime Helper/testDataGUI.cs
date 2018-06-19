45a3d3a7035899f67b97a2c2efa663b0328a6f81
Valerie T. Heyerdahl <c418fan@gmail.com>
2018-06-19 19:31:13 -0400

    better GUI

--- Arduino Test/Assets/DataLogging/Scripts/Runtime Helper/testDataGUI.cs
+++ Arduino Test/Assets/DataLogging/Scripts/Runtime Helper/testDataGUI.cs
@@ -7,8 +7,10 @@ public class testDataGUI : MonoBehaviour {
 
     public Font font;
     public int fontSize = 12;
-    int totalLineCount = 0;
+    Vector2 totalLineSpace = new Vector2(0, 0);
+
     public List<string> controlsList;
+    public List<string> actionList = new List<string>(5); ///// MAKE THE LIST WRAP
 
     // Use this for initialization
     void Start () {
@@ -33,16 +35,43 @@ public class testDataGUI : MonoBehaviour {
             allControls += c + "\n";
         }
 
-        GUI.Label(new Rect(0, 0, 
-                            200 * fontSize / 1.7f, controlsList.Count * 100 * fontSize / 1.7f), 
+        GUI.Label(new Rect(0, 
+                            0, 
+                            200 * fontSize / 1.7f, 
+                            controlsList.Count * 100 * fontSize / 1.7f), 
                             allControls);
+
+        totalLineSpace.x += controlsList.Count * 100 * fontSize / 1.7f;
+
+        string allActions = "";
+        foreach (string a in actionList)
+        {
+            allActions += a + "\n";
+        }
+
+        ///// MAKE THE LIST WRAP
+        // Action List
+        GUI.skin.label.font = font;
+        GUI.color = Color.blue;
+        GUI.skin.label.fontSize = fontSize;
+        GUI.Label(new Rect(0, 
+                            (controlsList.Count * fontSize/1.7f), 
+                            200 * fontSize / 1.7f,
+                            totalLineSpace.x + 100), 
+                            "Action List");
+
+        totalLineSpace.x += 100;
 
         // Status stuff
         GUI.skin.label.font = font;
-        GUI.color = Color.green;
+        GUI.color = Color.black;
         GUI.skin.label.fontSize = fontSize;
-        GUI.Label(new Rect(0, (controlsList.Count * fontSize/1.7f), 200 * fontSize / 1.7f, 
-                            (controlsList.Count * 100) + 100), 
+        GUI.Label(new Rect(0,
+                            (controlsList.Count * fontSize / 1.7f),
+                            200 * fontSize / 1.7f,
+                            totalLineSpace.x + 100),
                             "What in the sweet heck?");
+
+        totalLineSpace.x = 0;
     }
 }

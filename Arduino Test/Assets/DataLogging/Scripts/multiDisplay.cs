847dafa515385cf039add4a9bd1fedfdc6d8f409
Valerie T. Heyerdahl <c418fan@gmail.com>
2018-07-24 17:33:52 -0400

    Displays resolved

--- Arduino Test/Assets/DataLogging/Scripts/multiDisplay.cs
+++ Arduino Test/Assets/DataLogging/Scripts/multiDisplay.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class multiDisplay : MonoBehaviour {
 
+	public List<GameObject> ovrDisplays;
+	public bool multiDisplays = false;
+
 	// Use this for initialization
 	void Start () {
-		foreach(Display d in Display.displays)
+
+		ovrDisplays = GameObject.FindGameObjectsWithTag("MainCamera").ToList();
+
+		if (multiDisplays)
 		{
-			d.Activate();
+			foreach (GameObject cam in ovrDisplays) 
+			{
+				cam.GetComponent<Camera>().targetDisplay = 1;
+			}
+
+			foreach (Display d in Display.displays)
+			{
+				d.Activate(d.systemWidth, d.systemHeight, 30);
+			}
+		}
+		else
+		{
+			UnityEngine.XR.XRSettings.showDeviceView = false;
+
+			foreach (GameObject cam in ovrDisplays) 
+			{
+				cam.GetComponent<Camera>().targetDisplay = 0;
+			}
 		}
 		
 	}
